9aa6f4b1d4fd1c78d42c93f1ca62d87d92c9be13
Artur Rejment <arturrejment1@gmail.com>
2021-06-09 21:41:53 +0200

    Handling all the POST/GET/DELETE methods

--- todoapp/api/urls.py
+++ todoapp/api/urls.py
@@ -5,4 +5,9 @@ from . import views
 
 urlpatterns = [
     path('', views.apiOverview, name="api-overview"),
+    path('task-list/', views.taskView, name="task-list"),
+    path('task-detail/<str:pk>/', views.detailTaskView, name="task-detail"),
+    path('task-update/<str:pk>/', views.updateTask, name="task-update"),
+    path('task-delete/<str:pk>/', views.deleteTask, name="task-delete"),
+    path('task-create/', views.createTask, name="create-task")
 ]
--- todoapp/api/views.py
+++ todoapp/api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import TaskSerializer
+from .models import Task
 
 
 @api_view(['GET'])
@@ -14,3 +16,46 @@ def apiOverview(request):
         'Delete': '/taks-delete/<str:pk>/',
     }
     return Response(api_urls)
+
+
+@api_view(['GET'])
+def taskView(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def detailTaskView(request, **kwargs):
+    task = Task.objects.get(id=kwargs['pk'])
+    serializer = TaskSerializer(task, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def createTask(request):
+    serializer = TaskSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def updateTask(request, **kwargs):
+    task = Task.objects.get(id=kwargs['pk'])
+    serializer = TaskSerializer(instance=task, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def deleteTask(request, **kwargs):
+    task = Task.objects.get(id=kwargs['pk'])
+    task.delete()
+
+    return Response()
